2f0ccca031ded8020ccf5c958b576c6784447fcc
ishini <dimanij-se20032@stu.kln.ac.lk>
2026-03-04 21:49:35 +0530

    Production ready

--- backend/server.js
+++ backend/server.js
@@ -4,21 +4,27 @@ const express = require("express");
 const cors = require("cors");
 
 const oauthRoutes = require("./routes/oauthRoutes");
-const jiraRoutes = require("./routes/jiraRoutes"); // fetch/generate/push
+const jiraRoutes = require("./routes/jiraRoutes");
 const exportRoutes = require("./routes/exportRoutes");
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  }),
+);
 
+app.use(express.json());
 app.use("/api/oauth", oauthRoutes);
 app.use("/api", jiraRoutes);
-app.use("/api", exportRoutes);
 app.use("/api", require("./routes/exportRoutes"));
 
-app.get("/", (req, res) => res.send("Backend Running ✅"));
+app.get("/", (req, res) => res.send("Backend Running"));
 
-app.listen(process.env.PORT || 5000, () =>
-  console.log("Server running on port 5000"),
-);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
